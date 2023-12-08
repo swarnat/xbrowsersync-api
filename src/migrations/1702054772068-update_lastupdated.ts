82f372bc41d646fdec7047fbe919b573b5a4e51f
Stefan Warnat <ich@stefanwarnat.de>
2023-12-08 18:43:30 +0100

    fix empty down method

--- src/migrations/1702054772068-update_lastupdated.ts
+++ src/migrations/1702054772068-update_lastupdated.ts
@@ -18,5 +18,16 @@ export class UpdateLastupdated1702054772068 implements MigrationInterface {
     // await queryRunner.commitTransaction();
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // await queryRunner.startTransaction();
+
+    await queryRunner.dropColumn('bookmarks', 'lastUpdated');
+    await queryRunner.addColumn(
+      'bookmarks',
+      new TableColumn({
+        name: 'lastUpdated',
+        type: 'datetime',
+      })
+    );
+  }
 }
